9749b4b5a2cde8e9b849bc85839e2ea65db3cb54
jacshark <42828131+jacshark@users.noreply.github.com>
2021-07-28 09:28:14 +1000

    Add files via upload

---
+++ code/baseline_models/Bi-Model-Intent-And-Slot-master/data2index_ver2.py
@@ -0,0 +1,70 @@
+from make_dict import word_dict, intent_dict, slot_dict
+from make_dict import convert_int
+import config as cfg
+
+def makeindex(filename):
+    train_data = []
+    with open(filename) as f:
+        for line in f.readlines():
+            line = line.strip().split()
+            sample_sentence = []
+            sample_slot = []
+            # print('line:',line)
+            for index, item in enumerate(line):
+                word = item.split('<::>')[0] 
+
+                if word == '<=>':
+                    real_length = index
+                    break
+                if convert_int(word) is not None:
+                    word =  'DIGIT' * len(word)
+                else:
+                    pass
+                # print(intent_dict)
+                slot = item.split('<::>')[1]
+                if word in word_dict:
+                    sample_sentence.append(word_dict[word])
+                else:
+                    sample_sentence.append(word_dict['UNK'])
+                try:
+                  sample_slot.append(slot_dict[slot])
+                  # print('slot:',slot)
+                except:
+                  sample_slot.append(slot_dict['UNK'])
+                try:
+                  train_intent = intent_dict[ line[-1].split(';')[0] ]
+                  # print('train_intent:',line[-1].split(';')[0])
+                except:
+                  train_intent = intent_dict['UNK']
+
+            while len(sample_sentence) < cfg.max_len:
+                sample_sentence.append(word_dict['PAD'])
+            while len(sample_slot) < cfg.max_len:
+                sample_slot.append(slot_dict['O'])
+            if len(sample_sentence) > cfg.max_len:
+              sample_sentence = sample_sentence[:cfg.max_len]
+              sample_slot = sample_slot[:cfg.max_len]
+              real_length = cfg.max_len
+            # print('sample_sentence:',len(sample_sentence))
+            # print('real_length:',real_length)
+            # print('sample_slot:',len(sample_slot))
+            # print('train_intent:',train_intent)
+            # print(1/0)
+            train_data.append([sample_sentence, real_length, sample_slot, train_intent])
+    return train_data
+
+
+train_data = makeindex(cfg.train_file)
+test_data = makeindex(cfg.test_file)
+
+index2slot_dict = {}
+for key in slot_dict:
+    index2slot_dict[slot_dict[key]] = key
+
+
+print('Number of training samples: ', len(train_data))
+print('Number of test samples: ', len(test_data))
+print('Number of words: ', len(word_dict))
+print('Number of intent labels: ', len(intent_dict))
+print('Number of slot labels', len(slot_dict))
+
---
+++ code/baseline_models/Bi-Model-Intent-And-Slot-master/make_dict.py
@@ -0,0 +1,51 @@
+import config as cfg
+
+def convert_int(arr):
+    try:
+        a = int(arr)
+    except:
+        return None
+    return a
+
+# Make words dict	
+words = []
+with open(cfg.train_file) as f:
+    for line in f.readlines():
+        line = line.strip().lower().split()
+
+        for index, item in enumerate(line):
+            word = item.split('<::>')[0]
+            if word == '<=>':
+                break
+            if convert_int(word) is not None:
+                words.append('DIGIT' * len(word))
+            else:        
+                words.append(word)
+
+words_vocab = sorted(set(words))
+word_dict = {'UNK': 0, 'PAD': 1}
+
+for i, item in enumerate(words_vocab):
+    word_dict[item] = i + 2
+
+# Make slot tag dict 
+slot_dict = {}
+
+with open(cfg.vocab_slot_file) as f:
+
+    for i, line in enumerate(f.readlines()):
+        slot_dict[line.strip()] = i
+
+
+# print(slot_dict)
+
+# Make intent dict 
+intent_dict = {}
+
+with open(cfg.vocab_intent_file) as f:
+    for i, line in enumerate(f.readlines()):
+        intent_dict[line.strip()] = i
+
+# print(intent_dict)
+
+
